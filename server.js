a8ff64fb103e919cb1e6a599816dd1d2943928d7
Muhammad Zeeshan Khan <zeeshankhanaptech@gmail.com>
2019-01-21 12:28:36 +0500

    add cors in server for cors enable

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mongoose = require('./Config/db');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', ()=> console.log("we are Connected") );
 
-
+app.use(cors())
 
 app.listen(process.env.POST || 3000,(req,res)=>console.log("server run") );
+
 
 
 app.use(express.json());
